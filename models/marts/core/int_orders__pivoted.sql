2d8227e2bea0e1410ca4c934c27985c8b76ea494
Annant Sharma <sharmaannant05@gmail.com>
2023-10-03 16:30:35 +0000

    Pivot commit

--- models/marts/core/int_orders__pivoted.sql
+++ models/marts/core/int_orders__pivoted.sql
@@ -1,20 +1,24 @@
-{% set payment_methods = ['bank_transfer', 'coupon', 'credit_card', 'gift_card'] %}
+{%- set payment_methods = ['bank_transfer', 'credit_card', 'coupon', 'gift_card'] -%}
 
-with payments as(
-    select * from {{ ref('stg_payments') }}
+with payments as (
+   select * from {{ ref('stg_payments') }}
 ),
 
-pivoted as(
-    select 
-        order_id,
-        {% for payment_method in payment_methods %}
-            sum(case when payment_method = '{{payment_method}}' then amount else 0 end) as {{payment_method}}_amount
-            {% if not loop.last %}
-                ,
-            {% endif %}
-        {% endfor %}      
-    from payments
-    group by 1
+final as (
+   select
+       order_id,
+       {% for payment_method in payment_methods -%}
+
+       sum(case when payment_method = '{{ payment_method }}' then amount else 0 end)
+            as {{ payment_method }}_amount
+
+       {%- if not loop.last -%}
+         ,
+       {% endif -%}
+
+       {%- endfor %}
+   from {{ ref('stg_payments') }}
+   group by 1
 )
 
-select * from pivoted
+select * from final
